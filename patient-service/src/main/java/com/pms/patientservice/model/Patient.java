a72a16349dbcb444080efa0f5744af494f1afb96
smThilshan <smthilshan@gmail.com>
2025-10-01 11:50:07 +0530

    initial commit

---
+++ patient-service/src/main/java/com/pms/patientservice/model/Patient.java
@@ -0,0 +1,31 @@
+package com.pms.patientservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+    @NotNull
+    private LocalDate dateOfBirth;
+    @NotNull
+    private LocalDate registrationDate;
+
+}
