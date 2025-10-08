abf057757657b3d1fad32078eb89bdf496b9ff59
smThilshan <smthilshan@gmail.com>
2025-10-08 17:13:48 +0530

    added exception hanlding

--- patient-service/src/main/java/com/pms/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pms/patientservice/service/PatientService.java
@@ -2,6 +2,7 @@ package com.pms.patientservice.service;
 
 import com.pms.patientservice.dto.PatientRequestDTO;
 import com.pms.patientservice.dto.PatientResponseDTO;
+import com.pms.patientservice.exception.EmailAlreadyExistsException;
 import com.pms.patientservice.mapper.PatientMapper;
 import com.pms.patientservice.model.Patient;
 import com.pms.patientservice.repository.PatientRepository;
@@ -25,7 +26,13 @@ public class PatientService {
     }
 
     public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+        if (patientRepository.existsByEmail(patientRequestDTO.getEmail())) {
+            throw new EmailAlreadyExistsException("A patient with this email already exists" + patientRequestDTO.getEmail());
+        }
+
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
         return PatientMapper.convertToDTO(newPatient);
     }
+
+
 }
